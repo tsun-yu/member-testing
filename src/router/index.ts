407e912e4f2581323fa5223097ce0376907e6c46
Wayne <tsun0227@yahoo.com.tw>
2024-12-14 14:33:07 +0800

    delete route of loginView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'HomeView',
     component: HomeView,
-  },
-  {
-    path: '/login',
-    name: 'LoginView',
-    component: LoginView
   }
 ]
 
